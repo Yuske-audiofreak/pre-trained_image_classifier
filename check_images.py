d5c2c270033b0f1121ac3e3b50279d3fff118ecf
yusuke aita <yuskeaita@icloud.com>
2020-11-16 22:33:37 +0900

    Import time, sleep module from time

--- check_images.py
+++ check_images.py
@@ -1,3 +1,8 @@
 # check_images.py
 
 # TODO: 0: Timing Code
+# Name: Yusuke Aita
+# The date I started working on this project: 16th of November
+
+# Imports time() and sleep() fucntions from time module
+from time import time, sleep
