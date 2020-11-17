f01997db2e8b8b41e574d905663d1e6e7c36f8da
yusuke aita <yuskeaita@icloud.com>
2020-11-17 22:42:55 +0900

    Computes overall runtime in seconds

--- check_images.py
+++ check_images.py
@@ -6,3 +6,20 @@
 
 # Imports time() and sleep() fucntions from time module
 from time import time, sleep
+
+# Sets start time
+start_time = time()
+
+# Replace sleep (75) below with code you want to start_time
+sleep(75)
+
+# Sets end time
+end_time = time()
+
+# Computes overall runtime in seconds
+tot_time = start_time - end_time
+
+print("start time: {}, end time: {}, tot-time: {}".format(start_time, end_time, tot_time))
+
+# Prints overall runtime in seconds
+print("\nTotal Elapsed Runtime:", tot_time, "in seconds.")
