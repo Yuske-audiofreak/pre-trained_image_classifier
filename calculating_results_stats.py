3713e4ec34189a58c715bc376e74647f69f4d73d
Yusuke <>
2020-12-28 23:22:24 +0900

    'if' statement was organised

--- calculating_results_stats.py
+++ calculating_results_stats.py
@@ -97,17 +97,18 @@ def calculates_results_stats(results_dic):
         if results_dic[key][3] == 1:
             results_stats_dic['n_dogs_img'] += 1
 
+            if results_dic[key][4] == 1:
+                results_stats_dic['n_correct_dogs'] += 1
+
         if results_dic[key][2] == 1:
             results_stats_dic['n_match'] += 1
 
-        if results_dic[key][3] == 1 and results_dic[key][4] == 1:
-            results_stats_dic['n_correct_dogs'] += 1
+            if results_dic[key][3] == 1:
+                results_stats_dic['n_correct_breed'] += 1
 
         if results_dic[key][3] == 0 and results_dic[key][4] == 0:
             results_stats_dic['n_correct_notdogs'] += 1
 
-        if results_dic[key][2] == 1 and results_dic[key][3] == 1:
-            results_stats_dic['n_correct_breed'] += 1
 
     results_stats_dic['n_notdogs_img'] = results_stats_dic['n_images'] - results_stats_dic['n_dogs_img']
 
